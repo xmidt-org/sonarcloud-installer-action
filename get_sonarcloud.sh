21d1b10757ad10a326c773c52dd2317795f97952
schmidtw <weston_schmidt@alumni.purdue.edu>
2021-12-23 08:16:55 -0800

    Add more debugging before exiting/failing.

--- get_sonarcloud.sh
+++ get_sonarcloud.sh
@@ -6,18 +6,39 @@ getwrapper() {
     # Sadly sonarcloud doesn't provide any way to validate the integrity of this zip
     # file: https://community.sonarsource.com/t/sha256-checksum-for-build-wrapper-linux-x86-zip/43357/10
     curl -s -L -O https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
+    if [[ $? -ne 0 ]]; then
+        echo "Unable to download the build-wrapper-linux-x86.zip file."
+        exit 1
+    fi
     unzip -q -o build-wrapper-linux-x86.zip
+    if [[ $? -ne 0 ]]; then
+        echo "Unable to unzip the build-wrapper-linux-x86.zip file."
+        exit 1
+    fi
 }
 
 getcli() {
     # Get the latest cli_version of the scanning tool
     SONAR_VERSION=`curl -s https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/ |grep -o "sonar-scanner-cli-[0-9.]*-linux.zip"|grep ".*-cli-$cli_version.*-linux.zip"|sort -r|uniq|head -n 1`
     echo "Sonar-scanner-cli cli_version: ${SONAR_VERSION}"
+    if [[ -z $SONAR_VERSION ]]; then
+        echo "Unable to find any versions that match the specified version '${cli_version}'"
+        exit 1
+    fi
 
     curl -s -L -O https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/$SONAR_VERSION
+    if [[ $? -ne 0 ]]; then
+        echo "Unable to download the sonar-scanner-cli."
+        exit 1
+    fi
 
     # Get the sha256sum and validate the download
     curl -s -L -O https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/$SONAR_VERSION.sha256
+    if [[ $? -ne 0 ]]; then
+        echo "Unable to download the sonar-scanner-cli sha256 file."
+        exit 1
+    fi
+
     echo "  $SONAR_VERSION" >> $SONAR_VERSION.sha256
     sha256sum -c $SONAR_VERSION.sha256
     if [[ $? -ne 0 ]]
@@ -25,6 +46,10 @@ getcli() {
         exit 1
     fi
     unzip -q $SONAR_VERSION
+    if [[ $? -ne 0 ]]; then
+        echo "Unable to unzip the sonar-scanner-cli file."
+        exit 1
+    fi
 
     # Rename the resulting file to a common file name 'sonar-scanner' for simplicty.
     output=`ls | grep -o "sonar-scanner-[0-9.]*-linux"`
